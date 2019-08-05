6bdc513f02a31b87f36e0b6f1500c7d93ad0ddc3
Joshua <jds13245@hotmail.com>
2019-08-05 09:39:37 -0400

    Nearly completed test cases successfully

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,43 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+
 public class Solution {
+    // 1. Solve the unique word problem
+    // 2. Format it correctly
 
     public String countUniqueWords(String input){
-        return null;
+        String solu = "";
+
+        HashMap<String, Integer> tracker = new HashMap<String, Integer>();
+        String modifiedInput = input.replace(".", "");
+        String moreModifiedInput = modifiedInput.replaceAll("!", "");
+        String[] stringedInput = moreModifiedInput.split(" ");
+
+        for(String s : stringedInput) {
+            if(!tracker.containsKey(s)) {
+                tracker.put(s, 1);
+            }
+            else {
+                int currentValue = tracker.get(s);
+                currentValue++;
+                tracker.put(s, currentValue);
+            }
+        }
+
+        solu += String.format("The %d unique words are:\n", tracker.size());
+        int size = 0;
+        for (String s : tracker.keySet()) {
+            size++;
+            int value = tracker.get(s);
+            if (size < tracker.size()) {
+                solu += String.format("%s (Seen %d)\n", s, value);
+            }
+            else {
+                solu += String.format("%s (Seen %d)", s, value);
+            }
+        }
+
+        return solu;
     }
 }
